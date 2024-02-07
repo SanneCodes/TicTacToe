364cc038a6ba30838ebe4825e65286b01de550d1
SanneCodes <143067226+SanneCodes@users.noreply.github.com>
2024-02-07 09:07:29 +0100

    added project files

---
+++ TicTacToe/TicTacToe.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Bondesjakk_ny
+{
+    partial class TicTacToeUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblBox1 = new System.Windows.Forms.Label();
+            this.LblBox2 = new System.Windows.Forms.Label();
+            this.LblBox6 = new System.Windows.Forms.Label();
+            this.LblBox9 = new System.Windows.Forms.Label();
+            this.LblBox7 = new System.Windows.Forms.Label();
+            this.LblBox8 = new System.Windows.Forms.Label();
+            this.LblBox5 = new System.Windows.Forms.Label();
+            this.LblBox4 = new System.Windows.Forms.Label();
+            this.LblBox3 = new System.Windows.Forms.Label();
+            this.BtnAvbryt = new System.Windows.Forms.Button();
+            this.BtnNyttForsøk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LblBox1
+            // 
+            this.LblBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LblBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblBox1.Location = new System.Drawing.Point(38, 40);
+            this.LblBox1.Name = "LblBox1";
+            this.LblBox1.Size = new System.Drawing.Size(100, 100);
+            this.LblBox1.TabIndex = 2;
+            this.LblBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LblBox1.Click += new System.EventHandler(this.LblBox1_Click);
+            this.LblBox1.DoubleClick += new System.EventHandler(this.LblBox1_DoubleClick);
+            // 
+            // LblBox2
+            // 
+            this.LblBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LblBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblBox2.Location = new System.Drawing.Point(144, 40);
+            this.LblBox2.Name = "LblBox2";
+            this.LblBox2.Size = new System.Drawing.Size(100, 100);
+            this.LblBox2.TabIndex = 3;
+            this.LblBox2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LblBox2.Click += new System.EventHandler(this.LblBox2_Click);
+            this.LblBox2.DoubleClick += new System.EventHandler(this.LblBox2_DoubleClick);
+            // 
+            // LblBox6
+            // 
+            this.LblBox6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LblBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblBox6.Location = new System.Drawing.Point(250, 149);
+            this.LblBox6.Name = "LblBox6";
+            this.LblBox6.Size = new System.Drawing.Size(100, 100);
+            this.LblBox6.TabIndex = 4;
+            this.LblBox6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LblBox6.Click += new System.EventHandler(this.LblBox6_Click);
+            this.LblBox6.DoubleClick += new System.EventHandler(this.LblBox6_DoubleClick);
+            // 
+            // LblBox9
+            // 
+            this.LblBox9.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LblBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblBox9.Location = new System.Drawing.Point(250, 258);
+            this.LblBox9.Name = "LblBox9";
+            this.LblBox9.Size = new System.Drawing.Size(100, 100);
+            this.LblBox9.TabIndex = 5;
+            this.LblBox9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LblBox9.Click += new System.EventHandler(this.LblBox9_Click);
+            this.LblBox9.DoubleClick += new System.EventHandler(this.LblBox9_DoubleClick);
+            // 
+            // LblBox7
+            // 
+            this.LblBox7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LblBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblBox7.Location = new System.Drawing.Point(38, 258);
+            this.LblBox7.Name = "LblBox7";
+            this.LblBox7.Size = new System.Drawing.Size(100, 100);
+            this.LblBox7.TabIndex = 6;
+            this.LblBox7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LblBox7.Click += new System.EventHandler(this.LblBox7_Click);
+            this.LblBox7.DoubleClick += new System.EventHandler(this.LblBox7_DoubleClick);
+            // 
+            // LblBox8
+            // 
+            this.LblBox8.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LblBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblBox8.Location = new System.Drawing.Point(144, 258);
+            this.LblBox8.Name = "LblBox8";
+            this.LblBox8.Size = new System.Drawing.Size(100, 100);
+            this.LblBox8.TabIndex = 7;
+            this.LblBox8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LblBox8.Click += new System.EventHandler(this.LblBox8_Click);
+            this.LblBox8.DoubleClick += new System.EventHandler(this.LblBox8_DoubleClick);
+            // 
+            // LblBox5
+            // 
+            this.LblBox5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LblBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblBox5.Location = new System.Drawing.Point(144, 149);
+            this.LblBox5.Name = "LblBox5";
+            this.LblBox5.Size = new System.Drawing.Size(100, 100);
+            this.LblBox5.TabIndex = 8;
+            this.LblBox5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LblBox5.Click += new System.EventHandler(this.LblBox5_Click);
+            this.LblBox5.DoubleClick += new System.EventHandler(this.LblBox5_DoubleClick);
+            // 
+            // LblBox4
+            // 
+            this.LblBox4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LblBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblBox4.Location = new System.Drawing.Point(38, 149);
+            this.LblBox4.Name = "LblBox4";
+            this.LblBox4.Size = new System.Drawing.Size(100, 100);
+            this.LblBox4.TabIndex = 9;
+            this.LblBox4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LblBox4.Click += new System.EventHandler(this.LblBox4_Click);
+            this.LblBox4.DoubleClick += new System.EventHandler(this.LblBox4_DoubleClick);
+            // 
+            // LblBox3
+            // 
+            this.LblBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LblBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblBox3.Location = new System.Drawing.Point(250, 40);
+            this.LblBox3.Name = "LblBox3";
+            this.LblBox3.Size = new System.Drawing.Size(100, 100);
+            this.LblBox3.TabIndex = 10;
+            this.LblBox3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LblBox3.Click += new System.EventHandler(this.LblBox3_Click);
+            this.LblBox3.DoubleClick += new System.EventHandler(this.LblBox3_DoubleClick);
+            // 
+            // BtnAvbryt
+            // 
+            this.BtnAvbryt.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnAvbryt.Location = new System.Drawing.Point(417, 242);
+            this.BtnAvbryt.Name = "BtnAvbryt";
+            this.BtnAvbryt.Size = new System.Drawing.Size(127, 36);
+            this.BtnAvbryt.TabIndex = 12;
+            this.BtnAvbryt.Text = "Cancel";
+            this.BtnAvbryt.UseVisualStyleBackColor = false;
+            this.BtnAvbryt.Click += new System.EventHandler(this.BtnAvbryt_Click);
+            // 
+            // BtnNyttForsøk
+            // 
+            this.BtnNyttForsøk.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnNyttForsøk.Location = new System.Drawing.Point(417, 183);
+            this.BtnNyttForsøk.Name = "BtnNyttForsøk";
+            this.BtnNyttForsøk.Size = new System.Drawing.Size(127, 36);
+            this.BtnNyttForsøk.TabIndex = 11;
+            this.BtnNyttForsøk.Text = "Try again";
+            this.BtnNyttForsøk.UseVisualStyleBackColor = false;
+            this.BtnNyttForsøk.Click += new System.EventHandler(this.BtnNyttForsøk_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(366, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 45);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "For \"X\" click 1 time";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(366, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 45);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "For \"O\" click 2 times";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TicTacToeUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(632, 384);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnAvbryt);
+            this.Controls.Add(this.BtnNyttForsøk);
+            this.Controls.Add(this.LblBox3);
+            this.Controls.Add(this.LblBox4);
+            this.Controls.Add(this.LblBox5);
+            this.Controls.Add(this.LblBox8);
+            this.Controls.Add(this.LblBox7);
+            this.Controls.Add(this.LblBox9);
+            this.Controls.Add(this.LblBox6);
+            this.Controls.Add(this.LblBox2);
+            this.Controls.Add(this.LblBox1);
+            this.Name = "TicTacToeUI";
+            this.Text = "Bondesjakk";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblBox1;
+        private System.Windows.Forms.Label LblBox2;
+        private System.Windows.Forms.Label LblBox6;
+        private System.Windows.Forms.Label LblBox9;
+        private System.Windows.Forms.Label LblBox7;
+        private System.Windows.Forms.Label LblBox8;
+        private System.Windows.Forms.Label LblBox5;
+        private System.Windows.Forms.Label LblBox4;
+        private System.Windows.Forms.Label LblBox3;
+        private System.Windows.Forms.Button BtnAvbryt;
+        private System.Windows.Forms.Button BtnNyttForsøk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TicTacToe/TicTacToe.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bondesjakk_ny
+{
+    public partial class TicTacToeUI : Form
+    {
+        public TicTacToeUI()
+        {
+            InitializeComponent();
+        }
+
+
+        //Click
+        private void LblBox1_Click(object sender, EventArgs e)
+        {
+            LblBox1.Text = "x";
+        }
+
+        private void LblBox2_Click(object sender, EventArgs e)
+        {
+            LblBox2.Text = "x";
+        }
+
+        private void LblBox3_Click(object sender, EventArgs e)
+        {
+            LblBox3.Text = "x";
+        }
+
+        private void LblBox4_Click(object sender, EventArgs e)
+        {
+            LblBox4.Text = "x";
+        }
+
+        private void LblBox5_Click(object sender, EventArgs e)
+        {
+            LblBox5.Text = "x";
+        }
+
+        private void LblBox6_Click(object sender, EventArgs e)
+        {
+            LblBox6.Text = "x";
+        }
+
+        private void LblBox7_Click(object sender, EventArgs e)
+        {
+            LblBox7.Text = "x";
+        }
+
+        private void LblBox8_Click(object sender, EventArgs e)
+        {
+            LblBox8.Text = "x";
+        }
+
+        private void LblBox9_Click(object sender, EventArgs e)
+        {
+            LblBox9.Text = "x";
+        }
+
+        //Doubleclick
+
+        private void LblBox1_DoubleClick(object sender, EventArgs e)
+        {
+            LblBox1.Text = "o";
+        }
+
+        private void LblBox2_DoubleClick(object sender, EventArgs e)
+        {
+            LblBox2.Text = "o";
+        }
+
+        private void LblBox3_DoubleClick(object sender, EventArgs e)
+        {
+            LblBox3.Text = "o";
+        }
+
+        private void LblBox4_DoubleClick(object sender, EventArgs e)
+        {
+            LblBox4.Text = "o";
+        }
+
+        private void LblBox5_DoubleClick(object sender, EventArgs e)
+        {
+            LblBox5.Text = "o";
+        }
+
+        private void LblBox6_DoubleClick(object sender, EventArgs e)
+        {
+            LblBox6.Text = "o";
+        }
+
+        private void LblBox7_DoubleClick(object sender, EventArgs e)
+        {
+            LblBox7.Text = "o";
+        }
+
+        private void LblBox8_DoubleClick(object sender, EventArgs e)
+        {
+            LblBox8.Text = "o";
+        }
+
+        private void LblBox9_DoubleClick(object sender, EventArgs e)
+        {
+            LblBox9.Text = "o";
+        }
+
+        // Extra
+
+        private void BtnAvbryt_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnNyttForsøk_Click(object sender, EventArgs e)
+        {
+            LblBox1.Text = string.Empty;
+            LblBox2.Text = string.Empty;
+            LblBox3.Text = string.Empty;
+            LblBox4.Text = string.Empty;
+            LblBox5.Text = string.Empty;
+            LblBox6.Text = string.Empty;
+            LblBox7.Text = string.Empty;
+            LblBox8.Text = string.Empty;
+            LblBox9.Text = string.Empty;
+        }
+
+    }
+}
